a0d0352acf4686b004580ae516a179bce8b7939a
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-09-16 01:47:10 +0530

    initial commit :: jpa-crud

---
+++ jpa-crud/src/main/java/narayan/sbroutine/jpacrud/app/JpaCrudApplication.java
@@ -0,0 +1,12 @@
+package narayan.sbroutine.jpacrud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCrudApplication.class, args);
+	}
+}
